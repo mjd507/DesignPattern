33d83035125d4975e753f04ecc443b72c65ec893
majiandong <836240219@qq.com>
2016-11-17 20:04:06 +0800

    Add AbstractFactory Pattern

---
+++ Java/AbstractFactory.java
@@ -0,0 +1,60 @@
+package JavaSE_Senior.designPattern;
+
+public class AbstractFactory {
+	public static void main(String[] args) {
+		AbsFactory factory = new NikeFactory();
+		factory.makeBalls();
+		factory.makeShoes();
+	}
+}
+
+/**
+ * 产品接口
+ */
+interface AbsBalls{
+
+}
+interface AbsShoes{
+
+}
+
+/**
+ * 具体产品
+ */
+class NikeBall implements AbsBalls{
+	public NikeBall() {
+		System.out.println("Nike篮球被生产了");
+	}
+}
+class NikeShoes implements AbsShoes{
+	public NikeShoes() {
+		System.out.println("Nike鞋被生产了");
+	}
+}
+
+/**
+ * 工厂接口
+ */
+interface AbsFactory{
+	AbsBalls makeBalls();
+	AbsShoes makeShoes();
+}
+
+/**
+ * 具体工厂
+ */
+class NikeFactory implements AbsFactory{
+
+	@Override
+	public AbsBalls makeBalls() {
+		return new NikeBall();
+	}
+
+	@Override
+	public AbsShoes makeShoes() {
+		return new NikeShoes();
+	}
+}
+
+
+
