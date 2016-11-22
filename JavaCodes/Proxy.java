f5a6a32e5926fb612289d1e1e9eb71ff4cccfcef
majiandong <836240219@qq.com>
2016-11-22 17:28:07 +0800

    add proxy pattern code

---
+++ JavaCodes/Proxy.java
@@ -0,0 +1,39 @@
+package JavaSE_Senior.DesignPattern.JavaCodes;
+
+public class Proxy {
+	public static void main(String[] args) {
+		Women women = new WangPo();
+		women.happyWithMan();
+	}
+}
+
+interface Women{
+	void happyWithMan();
+}
+
+class PanJinLian implements Women{
+
+	@Override
+	public void happyWithMan() {
+		System.out.println("make love with ximenqi");
+	}
+	
+}
+
+class WangPo implements Women{
+	private Women woman;
+	
+	public WangPo() {
+		this.woman = new PanJinLian();
+	}
+	
+	public WangPo(Women woman) {
+		this.woman = woman;
+	}
+
+	@Override
+	public void happyWithMan() {
+		woman.happyWithMan();
+	}
+	
+}
